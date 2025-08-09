010d17a37e65ed8ebc9b505e71c684726b47cda3
Ferhat İpek <ferhatipek.code@gmail.com>
2025-08-09 23:02:03 +0300

    Person add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Person from "./components/person"
+
 
 function App() {
 
 
   return (
-    <div>Hello World</div>
+    <div>
+      <h1>React Uyhulaması</h1>
+      <h2>Kişiler</h2>
+      <Person />
+    </div>
 
     
   )
---
+++ src/components/person.jsx
@@ -0,0 +1,11 @@
+function Person(){
+  return (
+<div>
+        <p>Ferhat İpek</p>
+        <p>Meslek: Yazılım</p>
+        <p>Rank: Commander</p>
+      </div>
+  )
+}
+
+export default Person;
